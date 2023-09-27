4d6aced57b596713f5dcc387fb779285e79ed2d2
정의진 <ashiram02@gmail.com>
2023-09-27 13:58:43 -0400

    test commit

---
+++ Components/testView.swift
@@ -0,0 +1,20 @@
+//
+//  testView.swift
+//  solmal
+//
+//  Created by Jin Jung on 2023/09/27.
+//
+
+import SwiftUI
+
+struct testView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct testView_Previews: PreviewProvider {
+    static var previews: some View {
+        testView()
+    }
+}
